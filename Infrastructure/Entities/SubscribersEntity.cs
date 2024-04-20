c45b8c92ddde34588486fadcba307af6fd506b79
Koragm3 <hasanserro@gmail.com>
2024-04-20 21:54:42 +0200

    Add project files.

---
+++ Infrastructure/Entities/SubscribersEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Infrastructure.Entities;
+
+public class SubscribersEntity
+{
+    [Key]
+    public string Email { get; set; }
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekinReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupWeekly { get; set; }
+    public bool Podcasts { get; set; }
+
+}
